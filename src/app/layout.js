37fea1871d9629268ec9b73f2d0c0acb90d953e7
Lance Liang <liangxianyi2011@gmail.com>
2023-07-07 20:38:38 -0400

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 import Footer from "./Footer";
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 function RootLayout({ children }) {
   return (
