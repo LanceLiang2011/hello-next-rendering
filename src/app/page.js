b186458c4ea1c222c0336dc71b6c046f9d8461f2
Lance Liang <liangxianyi2011@gmail.com>
2023-07-07 20:40:45 -0400

    remove component

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,9 @@
 import React from "react";
 
-import HitCounter from "./HitCounter";
-import Blur from "../components/Blur";
-
 function Home() {
   return (
     <main>
       <h1>Welcome</h1>
-      You are visitor number{" "}
-      <Blur>
-        <HitCounter />
-      </Blur>
     </main>
   );
 }
