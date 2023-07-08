a7ba46a57d928f2cd19d299e0cfc76b03c20c6ff
Lance Liang <liangxianyi2011@gmail.com>
2023-07-07 20:34:11 -0400

    Change dynamic back to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.css";
 import Footer from "./Footer";
+export const dynamic = "force-dynamic";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <Footer />
+      <body>
+        {children} <Footer />
+      </body>
     </html>
   );
 }
