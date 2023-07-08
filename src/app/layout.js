723b30f88993d8232fe67da62f855600422e823a
Lance Liang <liangxianyi2011@gmail.com>
2023-07-07 20:16:54 -0400

    footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles.css";
+import Footer from "./Footer";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Footer />
     </html>
   );
 }
